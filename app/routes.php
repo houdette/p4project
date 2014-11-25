e2382a84a69c1b43fd175b0249ca306de4d1b3c9
NewUser <houdette.ben@gmail.com>
2014-11-25 17:43:51 -0500

    set up environment,connected Laravel to DB, installed pre

--- app/routes.php
+++ app/routes.php
@@ -10,6 +10,24 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+Route::get('mysql-test', function() {
+
+    # Print environment
+    echo 'Environment: '.App::environment().'<br>';
+
+    # Use the DB component to select all the databases
+    $results = DB::select('SHOW DATABASES;');
+
+    # If the "Pre" package is not installed, you should output using print_r instead
+    echo Pre::render($results);
+
+});
+Route::get('/get-environment',function() {
+
+    echo "Environment: ".App::environment();
+
+});
+
 
 Route::get('/', function()
 {
