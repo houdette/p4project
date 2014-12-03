53bc65a48d132d71ea4c55a7a202ac02de35f0fd
NewUser <houdette.ben@gmail.com>
2014-12-03 05:59:37 -0500

    added authentication

--- app/routes.php
+++ app/routes.php
@@ -10,6 +10,84 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+
+// app/routes.php`:
+
+Route::get('/signup',
+    array(
+        'before' => 'guest',
+        function() {
+            return View::make('signup');
+        }
+    )
+);
+Route::post('/signup', 
+    array(
+        'before' => 'csrf', 
+        function() {
+
+            $user = new User;
+            $user->email    = Input::get('email');
+            $user->password = Hash::make(Input::get('password'));
+
+            # Try to add the user 
+            try {
+                $user->save();
+            }
+            # Fail
+            catch (Exception $e) {
+                return Redirect::to('/signup')->with('flash_message', 'Sign up failed; please try again.')->withInput();
+            }
+
+            # Log the user in
+            Auth::login($user);
+
+            return Redirect::to('/list')->with('flash_message', 'Welcome to Foobooks!');
+
+        }
+    )
+);
+
+Route::get('/login',
+    array(
+        'before' => 'guest',
+        function() {
+            return View::make('login');
+        }
+    )
+);
+
+Route::post('/login', 
+    array(
+        'before' => 'csrf', 
+        function() {
+
+            $credentials = Input::only('email', 'password');
+
+            if (Auth::attempt($credentials, $remember = true)) {
+                return Redirect::intended('/')->with('flash_message', 'Welcome Back!');
+            }
+            else {
+                return Redirect::to('/login')->with('flash_message', 'Log in failed; please try again.');
+            }
+
+            return Redirect::to('login');
+        }
+    )
+);
+
+# /app/routes.php
+Route::get('/logout', function() {
+
+    # Log out
+    Auth::logout();
+
+    # Send them to the homepage
+    return Redirect::to('/');
+
+});
+
+
 # /app/routes.php
 Route::get('/debug', function() {
 
---
+++ app/views/_master.blade.php
@@ -0,0 +1,15 @@
+ <!--<body>
+
+    @if(Session::get('flash_message'))
+        <div class='flash-message'>{{ Session::get('flash_message') }}</div>
+    @endif
+
+    [... Rest of your page ...]-->
+    
+    
+    
+  <!--  @if(Auth::check())
+    <a href='/logout'>Log out {{ Auth::user()->email; }}</a>
+@else 
+    <a href='/signup'>Sign up</a> or <a href='/login'>Log in</a>
+@endif -->
